706f3b1056e5fda9a4a560cd4758f5cef53a93a0
Nikolay Edigaryev <edigaryev@gmail.com>
2021-02-03 03:53:07 +0300

    Introduce TestBasicPatterns test

---
+++ glob_test.go
@@ -0,0 +1,47 @@
+package glob_test
+
+import (
+	"fmt"
+	glob "github.com/cirruslabs/go-java-glob"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBasicPatterns(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		Pattern     string
+		ShouldMatch []string
+		ShouldNotMatch []string
+	}{
+		{"*.txt", []string{"file.txt"}, []string{"file.py"}},
+		{"*.{html,htm}", []string{"new.html", "old.htm"}, []string{"file.py"}},
+		{"?.txt", []string{"1.txt"}, []string{"22.txt"}},
+		{"*.*", []string{"README.md"}, []string{"vmlinuz"}},
+		{"/etc/*", []string{"/etc/passwd"}, []string{"/boot/vmlinuz", "/etc/ssh/sshd_config"}},
+		{"/home/**", []string{"/home/user1", "/home/user2/www"}, []string{"/"}},
+		{"**.go", []string{"dir/file.go", "dir/subdir/file.go"}, []string{"dir/file.py"}},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(testCase.Pattern, func(t *testing.T) {
+			re, err := glob.ToRegexPattern(testCase.Pattern, false)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			fmt.Println(re.String())
+
+			for _, shouldMatch := range testCase.ShouldMatch {
+				assert.True(t, re.MatchString(shouldMatch))
+			}
+
+			for _, shouldMatch := range testCase.ShouldNotMatch {
+				assert.False(t, re.MatchString(shouldMatch))
+			}
+		})
+	}
+}
